b0116980a1366d8a9c00b7ab388a2ab7ef7b5672
bimsaragamage <gamagebimsara@gmail.com>
2021-08-18 09:07:43 +0530

    Finish singly linkedlist basic creation

--- src/SinglyLinkedList.tsx
+++ src/SinglyLinkedList.tsx
@@ -7,4 +7,27 @@ class SinglyLinkedList extends React.Component{
 
 }
 
+class node {
+    constructor(value:number);
+    constructor(value:number, previous_node:node);
+    constructor(...my_arr:any[]) {
+        this.value = my_arr[0]
+        this.pntr = Math.floor(Math.random()*10000)
+        if(my_arr.length===2){
+            my_arr[1].nextPntr = this.pntr
+        }
+    }
+
+    pntr = NaN
+    value = NaN
+    nextPntr = NaN
+
+    displayValues = () => {console.log("value: " + this.value + ", pointer: " + this.pntr + ", next pointer: " + this.nextPntr)}
+}
+
+let arr = [35, 6, 2, 9, 345, 1, 3, 0, 9]
+let obj_arr:node[] = []
+arr.map((value:number, key:number) => {key===0?obj_arr.push(new node(value)):obj_arr.push(new node(value, obj_arr[key-1]))})
+console.log(obj_arr)
+
 export default SinglyLinkedList;
